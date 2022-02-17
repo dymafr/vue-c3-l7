f5b538cc3d2d7fc0a8a7bef6116667dec5e2c8c4
dymafr <erwan1309@hotmail.com>
2022-02-17 12:39:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-wbs1ss
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-wbs1ss)
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 @click="userClick">Bonjour tout le monde !</h1>
+  <input @input="userInput" type="text" />
+</template>
+
+<script setup lang="ts">
+function userClick(event: MouseEvent) {
+  console.log(event);
+}
+
+function userInput(event: Event) {
+  const target = event.target as HTMLInputElement;
+  target.value = 'Bonjour';
+  console.log(event);
+}
+</script>
+
+<style></style>
