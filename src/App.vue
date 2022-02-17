f592ab83cd80acf587c92de27c83521f45412ac4
dymafr <erwan1309@hotmail.com>
2022-02-17 17:45:43 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
   <h1 @click="userClick">Bonjour tout le monde !</h1>
-  <input @input="userInput" type="text" />
 </template>
 
 <script setup lang="ts">
 function userClick(event: MouseEvent) {
-  console.log(event);
-}
-
-function userInput(event: Event) {
-  const target = event.target as HTMLInputElement;
-  target.value = 'Bonjour';
-  console.log(event);
+  console.log(event.);
 }
 </script>
 
