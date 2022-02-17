a7c646fee23dc8c60edad504e9f7ab52fb365bb1
dymafr <erwan1309@hotmail.com>
2022-02-17 17:47:19 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1 @click="userClick">Bonjour tout le monde !</h1>
+  <h1 @click="userClick.left.once">Bonjour tout le monde !</h1>
+  <form @submit.prevent="formSubmit">
+    <button>Envoyer</button>
+  </form>
 </template>
 
 <script setup lang="ts">
 function userClick(event: MouseEvent) {
-  console.log(event.);
+  console.log(event);
+}
+function formSubmit() {
+  console.log('Envoyé');
 }
 </script>
 
